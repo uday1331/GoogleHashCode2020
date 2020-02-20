ecc4b10076ae3491ec1f4a2c2ec25493962026d1
uday1331 <u3555276@connect.hku.hk>
2020-02-21 00:40:04 +0800

    change import names

--- solve.py
+++ solve.py
@@ -1,9 +1,9 @@
 import argparse
 import random
 from score import parse
-from utkarsh.main import printSquare as utkarshPrintSquare
-from waqas.main import printSquare as waqasPrintSquare
-from divyansh.main import printSquare as divyanshPrintSquare
+from uday.main import printSquare as udayPrintSquare
+from ahsan.main import printSquare as ahsanPrintSquare
+from welvin.main import printSquare as welvinPrintSquare
 from sean.main import printSquare as seanPrintSquare
 # inp is an input file as a single string
 # return your output as a string
@@ -14,9 +14,9 @@ def solve(seed, inp, log):
     ns = parse(inp)
 
     # TODO: Solve the problem
-    divyanshPrintSquare(1)
-    waqasPrintSquare(2)
-    utkarshPrintSquare(3)
+    udayPrintSquare(1)
+    ahsanPrintSquare(2)
+    welvinPrintSquare(3)
     seanPrintSquare(4)
 
     return '0'
