cd6068dad3bfb55e8034d21d2b9637a251132a45
uday1331 <u3555276@connect.hku.hk>
2020-02-21 05:19:57 +0800

    woohoo index

--- sean/util.py
+++ sean/util.py
@@ -2,7 +2,9 @@ def square(number):
     return number * number
 
 def sortLibraries(libraries,days,bookScores):
-    for library in libraries:
+    for idx,library in enumerate(libraries):
+        library["books"] = sorted(library["books"], key=lambda x: bookScores[x], reverse=True)
         library["score"] = float(sum([bookScores[x] for idx,x in enumerate(library["books"]) if (idx < (days-library["signUpDays"])*library["numOfShipsPerDay"])])) / days
+        library["id"] = idx
 
     return sorted(libraries, key=lambda x: x["score"], reverse=True)
