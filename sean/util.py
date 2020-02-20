ca0cb668fa7e07b616e5dc42d6ca88c3fe22becf
ahsan8244 <syed.ahsan8244@gmail.com>
2020-02-21 05:00:27 +0800

    ahsan: here u go

--- sean/util.py
+++ sean/util.py
@@ -1,2 +1,8 @@
 def square(number):
     return number * number
+
+def calculateScores(libraries,days,bookScores):
+    for library in libraries:
+        library["score"] = float(sum([bookScores[x] for idx,x in enumerate(library["books"]) if (idx < (days-library["signUpDays"])*library["numOfShipsPerDay"])])) / days
+
+    return sorted(libraries, key=lambda x: x["score"], reverse=True)
