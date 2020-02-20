761316296648b2c205ccb88e7809a00dbd994372
seanchok <seanchok@gmail.com>
2020-02-21 05:07:17 +0800

    rename sort utility

--- sean/util.py
+++ sean/util.py
@@ -1,7 +1,7 @@
 def square(number):
     return number * number
 
-def calculateScores(libraries,days,bookScores):
+def sortLibraries(libraries,days,bookScores):
     for library in libraries:
         library["score"] = float(sum([bookScores[x] for idx,x in enumerate(library["books"]) if (idx < (days-library["signUpDays"])*library["numOfShipsPerDay"])])) / days
 
